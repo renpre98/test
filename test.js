027edd6d39cb637f51201f2b997078a378c24a43
Rene Preikschat <reno.p@hotmail.de>
2018-08-28 15:53:05 +0200

    v0.0.2b Testing an updated World

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 function helloWorld()
 {
-	alert("Hello World"); //Open an Alert Window witch says something about the World.
+	var sToAlert = "";//Create the sToAlert String which should be written in Alert later.
+	if(true)
+	{
+		sToAlert="The World is not Enough"; //Add some Text which should be written along with the Hello
+	}
+	alert("Hello "+sToAlert); //Open an Alert Window witch says something containing "Hello".
+	
 	return "success"; //Return some useless Stuff.
 }
