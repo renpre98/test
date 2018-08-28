e4948963fb6de9380ee2d34b3e369c7750d3984a
Rene Preikschat <reno.p@hotmail.de>
2018-08-28 16:07:37 +0200

    v0.0.3a added a !

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function helloWorld()
 	var sToAlert = "";//Create the sToAlert String which should be written in Alert later.
 	if(true)
 	{
-		sToAlert="The World is not Enough"; //Add some Text which should be written along with the Hello
+		sToAlert="The World is not Enough!"; //Add some Text which should be written along with the Hello
 	}
 	alert("Hello "+sToAlert); //Open an Alert Window witch says something containing "Hello".
 	
